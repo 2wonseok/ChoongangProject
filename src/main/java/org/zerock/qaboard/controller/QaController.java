2980db8694ab7fb693e1fd3e54ea214025435df2
vender4401 <vender4401@gmail.com>
2021-02-03 13:51:17 +0900

    0203yoonkwan0151
    (cherry picked from commit 9c723d6bbfa8b37e52516acf700162c103f6ebce)

--- src/main/java/org/zerock/qaboard/controller/QaController.java
+++ src/main/java/org/zerock/qaboard/controller/QaController.java
@@ -1,14 +1,7 @@
 package org.zerock.qaboard.controller;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.zerock.qaboard.domain.QaVO;
-import org.zerock.qaboard.service.QaService;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
@@ -18,8 +11,5 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/qa_board/*")
 @Log4j
 public class QaController {
-	
-	private QaService service;	
-
 
 }
--- src/main/java/org/zerock/qaboard/mapper/QaMapper.java
+++ src/main/java/org/zerock/qaboard/mapper/QaMapper.java
@@ -1,12 +1,5 @@
 package org.zerock.qaboard.mapper;
 
-import java.util.List;
-
-import org.zerock.qaboard.domain.Criteria;
-import org.zerock.qaboard.domain.QaVO;
-
 public interface QaMapper {
 
-	public List<QaVO> getlist();
-	
 }
