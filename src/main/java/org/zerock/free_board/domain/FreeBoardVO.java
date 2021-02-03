0f9611e87cb6ab69176e8b0462cfad96f98f705c
HamDongJoo <epdwn1@gmail.com>
2021-02-03 09:46:14 +0900

    0203 9:46 commit dj
    (cherry picked from commit d5f9ee721b772d7ece819e7e55be6e53331a96f7)

---
+++ src/main/java/org/zerock/free_board/domain/FreeBoardVO.java
@@ -0,0 +1,13 @@
+package org.zerock.free_board.domain;
+
+public class FreeBoardVO {
+	private free_seq
+	free_title
+	free_content
+	free_writer
+	free_notice
+	free_readcnt
+	free_regdate
+	free_updatedate
+
+}
