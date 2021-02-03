d25e2e8d20bd9b8ce77d7ce551af163fc998339d
thirtist <thirtist@naver.com>
2021-02-03 11:33:54 +0900

    0203chulsu4
    (cherry picked from commit 8e218a2d777eb8f81ebdafd352df2caa9ec7bc6e)

--- src/main/java/org/zerock/product/mapper/ProductMapper.java
+++ src/main/java/org/zerock/product/mapper/ProductMapper.java
@@ -1,6 +1,7 @@
 package org.zerock.product.mapper;
 
+import org.zerock.product.domain.ProductVO;
 
-public class ProductMapper {
-
+public interface ProductMapper {
+	public void insert(ProductVO product);
 }
